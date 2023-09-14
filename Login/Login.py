e23dba852fb920a8a480c31495624699b4031b6c
YumuA <bons.santi@hotmail.com>
2023-09-13 20:21:10 -0500

    Update-Add passButton And db credentials

--- Login/Login.py
+++ Login/Login.py
@@ -1,11 +1,23 @@
 import sys
 from PyQt6.QtWidgets import (QApplication, QLabel,
-QWidget,QLineEdit, QPushButton,QMessageBox,QCheckBox)
-from PyQt6.QtGui import QFont, QPixmap
+QWidget,QLineEdit, QPushButton,QMessageBox,QCheckBox, QVBoxLayout)
+from PyQt6.QtGui import QFont, QPixmap 
 import mysql.connector
+
+
+class MainView(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Main View")
+        self.setGeometry(100, 100, 350, 250)
+        layout = QVBoxLayout()
+        welcome_label = QLabel("¡Bienvenido a la vista principal!")
+        layout.addWidget(welcome_label)
+        self.setLayout(layout)
 
 class Login(QWidget):
     
+    print("El clase login iniciada...")
 
     def __init__(self):
         super().__init__()
@@ -44,19 +56,44 @@ class Login(QWidget):
         self.check_view_password = QCheckBox(self)
         self.check_view_password.setText("Ver la contraseña")
         self.check_view_password.move (90,110)
+        self.check_view_password.toggled.connect(self.show_pass)
 
         login_button = QPushButton(self)
         login_button.setText("Login")
         login_button.setFont(QFont("Arial", 12))
         login_button.resize(120,40)
-        login_button.move(115,160)
+        login_button.move(90,140)
         login_button.clicked.connect(self.iniciar_sesion)
 
+
+        register_button = QPushButton(self)
+        register_button.setText("register")
+        register_button.setFont(QFont("Arial", 12))
+        register_button.resize(120,40)
+        register_button.move(90,180)
+        register_button.clicked.connect(self.registrar_usr)
+
+    def show_pass(self, clicked):
+        if clicked:
+            self.pass_input.setEchoMode(
+                QLineEdit.EchoMode.Normal
+                )
+        else:
+            self.pass_input.setEchoMode(
+                QLineEdit.EchoMode.Password
+                )
+
+    def iniciar(self):
+        pass
+
+    def registrar_usr(self):
+        pass
 
     def iniciar_sesion(self):
         usuario = self.user_input.text()
         contraseña = self.pass_input.text()
-        
+        pass
+
         try:
             conexion = mysql.connector.connect(
             host = 'localhost',
@@ -64,11 +101,23 @@ class Login(QWidget):
             password ='',
             database = 'test1'
         )
-
             if conexion.is_connected():
+                cursor = conexion.cursor(dictionary=True)
+                consulta = "SELECT * FROM usuarios WHERE usuario = %s AND contraseña = %s"
+                cursor.execute(consulta, (usuario, contraseña))
+                resultado = cursor.fetchone()
+
+
+            if resultado:
                 QMessageBox.information(self, "Exito", "Conectado Correctamente a MYSQL")
                 self.is_logged = True
                 conexion.close()
+
+                self.main_view = MainView()
+                self.main_view.show()
+                self.hide()
+
+
             else:
               QMessageBox.warning(self, "Error", "No se pudo conectar a MySQL")
         except mysql.connector.Error as error:
@@ -78,6 +127,8 @@ class Login(QWidget):
 if __name__ == "__main__":
     
     app = QApplication(sys.argv)
+    print("El inicio...")
 
     login = Login()
     sys.exit(app.exec())
+    
--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import sys 
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+#import sys 
+#from PyQt6.QtWidgets import QApplication, QWidget, QLabel
 
 
-class VentanaVacia(QWidget):
+#class VentanaVacia(QWidget):
 
-    def __init__(self):
-        super().__init__()
-        self.inicializarUI()
+ #   def __init__(self):
+  #      super().__init__()
+   #     self.inicializarUI()
     
-    def inicializarUI(self):
-        self.setGeometry(100, 100,250,250)
-        self.setWindowTitle("Hola test 1")
-        print("Funciona")
-        usr_label = QLabel(self)        
-        usr_label.setText("Usuario")
-        usr_label.move(20,54)
-        self.show()
+  #  def inicializarUI(self):
+   #     self.setGeometry(100, 100,250,250)
+    #    self.setWindowTitle("Hola test 1")
+     #   print("Funciona")
+      #  usr_label = QLabel(self)        
+       # usr_label.setText("Usuario")
+        #usr_label.move(20,54)
+        #self.show()
     
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ventana = VentanaVacia()
+#if __name__ == "__main__":
+ #   app = QApplication(sys.argv)
+  #  ventana = VentanaVacia()
 
-    sys.exit(app.exec())
+#    sys.exit(app.exec())
